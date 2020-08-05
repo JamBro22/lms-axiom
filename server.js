b08aaa6c98bce0f68a6ca91ec7cc929c5caff623
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-08-05 18:28:30 +0200

    Serve prod version of the app on heroku.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,11 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/courses", require("./routes/courses"));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+});
 
 const PORT = process.env.PORT || 5000;
 

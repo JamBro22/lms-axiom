526f7c3078bb80010c0e27717a15e15a4b0c83c3
Jamie <jmb.black3nd@gmail.com>
2020-08-11 12:59:05 +0200

    created add course btn and link

--- client/src/components/myCourses/MyCourses.js
+++ client/src/components/myCourses/MyCourses.js
@@ -3,6 +3,7 @@ import Header from "../layout/headers/Header";
 import CourseCard from "../../components/courses/CourseCard";
 import AuthContext from "../../context/auth/authContext";
 import CourseContext from "../../context/courses/courseContext";
+import { Link } from "react-router-dom";
 
 const MyCourses = () => {
   const authContext = useContext(AuthContext);
@@ -11,7 +12,13 @@ const MyCourses = () => {
 
   const courseContext = useContext(CourseContext);
 
-  const { courses, getCreatedCourses, loading, setCourse, deleteCourse } = courseContext;
+  const {
+    courses,
+    getCreatedCourses,
+    loading,
+    setCourse,
+    deleteCourse,
+  } = courseContext;
 
   useEffect(() => {
     getCreatedCourses();
@@ -23,22 +30,35 @@ const MyCourses = () => {
     // eslint-disable-next-line
   }, []);
 
-  return(
+  return (
     <Fragment>
       <Header heading="My Courses" />
       <div className="auth-box">
-        {loading ? <h1>Loading...</h1> : (
-          courses.length > 0 ? (
-            courses.map(course => (
-              <CourseCard course={course} button1="Edit" button2="Delete" type="danger" click={() => setCourse(course)} click2={() => deleteCourse(course._id) } key={course._id} />
-            ))
-          ) : (
-            <h3>No courses...</h3>
-          )
-        ) }
+        <Link to="/addcourse" className="add-btn-link">
+          <button type="button" className="btn btn-success add-btn">
+            Add Course
+          </button>
+        </Link>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : courses.length > 0 ? (
+          courses.map((course) => (
+            <CourseCard
+              course={course}
+              button1="Edit"
+              button2="Delete"
+              type="danger"
+              click={() => setCourse(course)}
+              click2={() => deleteCourse(course._id)}
+              key={course._id}
+            />
+          ))
+        ) : (
+          <h3>No courses...</h3>
+        )}
       </div>
     </Fragment>
-  )
+  );
 };
 
 export default MyCourses;

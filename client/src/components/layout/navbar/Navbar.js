6259f7e92af8571bf450dfcb6fd884e9315a7305
Jamie <jmb.black3nd@gmail.com>
2020-08-18 09:18:55 +0200

    adjusted navbar responsiveness

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -7,17 +7,17 @@ const Navbar = () => {
   const authContext = useContext(AuthContext);
   const { logout, isAuthenticated, user, token } = authContext;
 
-  const [ open, setOpen ] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const openMenu = () => {
-    open ? setOpen(false) : setOpen(true)
-  }
+    open ? setOpen(false) : setOpen(true);
+  };
 
   const closeMenu = () => {
     if (open) {
-      setOpen(false)
+      setOpen(false);
     }
-  }
+  };
 
   const icons = [
     "fab fa-facebook-f",
@@ -51,40 +51,48 @@ const Navbar = () => {
   const authLinks = (
     <Fragment>
       <i class="fas fa-bars menu-icon" onClick={openMenu}></i>
-    <nav className={open ? "auth-nav-small" : "close-nav" && "auth-nav"}>
-      <h1 className="londrina auth-logo transparent">
-        Axiom<span className="dot transparent">.</span>
-      </h1>
-      <h5 className="user center transparent">
-        {user && user.firstName} {user && user.lastName}
-      </h5>
-      <ul className="auth-nav-links transparent">
-        <NavLink to="/" className="auth-link-item" onClick={closeMenu}>
-          <li className="list-item transparent">Home</li>
-        </NavLink>
-        <NavLink to="/Dashboard" className="auth-link-item">
-          <li className="list-item transparent">Dashboard</li>
-        </NavLink>
-        <NavLink to="/Explore" className="auth-link-item">
-          <li className="list-item transparent">Explore</li>
-        </NavLink>
-        <NavLink to="/mycourses" className="auth-link-item">
-          <li className="list-item transparent">My Courses</li>
-        </NavLink>
-        <NavLink to="/login" onClick={onLogout} className="auth-link-item">
-          <li className="list-item transparent">Logout</li>
-        </NavLink>
-      </ul>
-      <ul className="nav-icon-list">
-        {icons.map((icon) => {
-          return (
-            <li className="icon-list-item" key={icon}>
-              <i className={`${icon} nav-icon`} />
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+      <nav className={open ? "auth-nav-small" : "close-nav" && "auth-nav"}>
+        <h1 className="londrina auth-logo transparent">
+          Axiom<span className="dot transparent">.</span>
+        </h1>
+        <h5 className="user center transparent">
+          {user && user.firstName} {user && user.lastName}
+        </h5>
+        <ul className="auth-nav-links transparent">
+          <NavLink to="/" className="auth-link-item" onClick={closeMenu}>
+            <li className="list-item transparent">Home</li>
+          </NavLink>
+          <NavLink
+            to="/Dashboard"
+            className="auth-link-item"
+            onClick={closeMenu}
+          >
+            <li className="list-item transparent">Dashboard</li>
+          </NavLink>
+          <NavLink to="/Explore" className="auth-link-item" onClick={closeMenu}>
+            <li className="list-item transparent">Explore</li>
+          </NavLink>
+          <NavLink
+            to="/mycourses"
+            className="auth-link-item"
+            onClick={closeMenu}
+          >
+            <li className="list-item transparent">My Courses</li>
+          </NavLink>
+          <NavLink to="/login" onClick={onLogout} className="auth-link-item">
+            <li className="list-item transparent">Logout</li>
+          </NavLink>
+        </ul>
+        <ul className="nav-icon-list">
+          {icons.map((icon) => {
+            return (
+              <li className="icon-list-item" key={icon}>
+                <i className={`${icon} nav-icon`} />
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </Fragment>
   );
 
